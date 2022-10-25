bb972f3fcd36f0ca3eccd2e605bc04e08c40a82f
not-dalia <32544547+not-dalia@users.noreply.github.com>
2022-10-25 12:09:11 +0100

    GAS Update calendar content

---
+++ src/data/categories.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {"1":{"id":1,"name":"Royal Mail Strikes","color":"#DC3232"},"2":{"id":2,"name":"Rail Strikes","color":"#165e98"},"3":{"id":3,"name":"TfL Strikes","color":"#388e3c"},"4":{"id":4,"name":"Bus Strikes","color":"#8e24aa"},"1000":{"id":1000,"name":"Other Strikes","color":"#e65100"}}
